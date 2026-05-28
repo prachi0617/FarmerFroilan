8b898974d0c2255e8c41808c73d9f6118a42ac02
gitanitraj <anitra.com@gmail.com>
2026-05-28 14:28:55 -0400

    Added test files

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/monday/MondayTest.java
@@ -0,0 +1,5 @@
+package com.zipcodewilmington.froilansfarm.monday;
+
+public class MondayTest {
+    
+}
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/services/FeedingServiceTest.java
@@ -0,0 +1,5 @@
+package com.zipcodewilmington.froilansfarm.services;
+
+public class FeedingServiceTest {
+    
+}
