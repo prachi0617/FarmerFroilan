f8d436dd965336da6119eebb732efd714a8f7f29
thejoeflynn <thejoeflynn@gmail.com>
2026-05-28 15:38:39 -0400

    added vehicle tests

--- src/test/java/com/zipcodewilmington/froilansfarm/vehicles/CropDusterTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/vehicles/CropDusterTest.java
@@ -1,5 +1,36 @@
 package com.zipcodewilmington.froilansfarm.vehicles;
 
-public class CropDusterTest {
-    
+import com.zipcodewilmington.froilansfarm.base.FarmTestBase;
+import org.junit.jupiter.api.Test;
+
+public class CropDusterTest extends FarmTestBase {
+
+    @Test
+    void cropDusterCanFly() {
+        cropDuster.fly();
+    }
+
+    @Test
+    void cropDusterCanOperate() {
+        cropDuster.operate(farm);
+    }
+
+    @Test
+    void cropDusterFertilizesCropRow() {
+        cropDuster.fertilize(cropRow);
+    }
+
+    @Test
+    void cropRowIsFertilizedAfterCropDusterPasses() {
+        cropDuster.fertilize(cropRow);
+        for (Crop crop : cropRow.getCrops()) {
+            assertTrue(crop.hasBeenFertilized());
+        }
+    }
+
+    @Test
+    void cropDusterMakesNoise() {
+        assertNotNull(cropDuster.makeNoise());
+    }
+
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/vehicles/TractorTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/vehicles/TractorTest.java
@@ -1,5 +1,29 @@
 package com.zipcodewilmington.froilansfarm.vehicles;
 
-public class TractorTest {
-    
+import com.zipcodewilmington.froilansfarm.base.FarmTestBase;
+import org.junit.jupiter.api.Test;
+
+public class TractorTest extends FarmTestBase {
+
+    @Test
+    void tractorCanOperate() {
+        tractor.operate(farm);
+    }
+
+    @Test
+    void tractorHarvestsCrop() {
+        tractor.harvest(crop);
+    }
+
+    @Test
+    void cropIsHarvestedAfterTractorHarvests() {
+        tractor.harvest(crop);
+        assertTrue(crop.isHarvested());
+    }
+
+    @Test
+    void tractorMakesNoise() {
+        assertNotNull(tractor.makeNoise());
+    }
+
 }
