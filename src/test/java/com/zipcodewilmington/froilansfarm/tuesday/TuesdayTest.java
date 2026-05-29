2d9b69cd16916651a131903ebb512a109b124d82
thejoeflynn <thejoeflynn@gmail.com>
2026-05-29 10:41:29 -0400

    Sunday Monday Tuesday

--- src/test/java/com/zipcodewilmington/froilansfarm/tuesday/TuesdayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/tuesday/TuesdayTest.java
@@ -1,5 +1,21 @@
 package com.zipcodewilmington.froilansfarm.tuesday;
 
-public class TuesdayTest {
-    
+import com.zipcodewilmington.froilansfarm.base.FarmTestBase;
+import com.zipcodewilmington.froilansfarm.crop.Crop;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TuesdayTest extends FarmTestBase {
+
+    @Test
+    void everyCropIsHarvestedAfterTractorRun() {
+        for (CropRow cropRow : farm.getField().getCropRows()) {
+            for (Crop crop : cropRow.getCrops()) {
+                tractor.harvest(crop);
+                assertTrue(crop.isHarvested());
+            }
+        }
+    }
+
 }
