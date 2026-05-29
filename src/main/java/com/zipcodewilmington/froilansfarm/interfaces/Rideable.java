8fab00c32b41323dc39026893fa2a90650546a38
thejoeflynn <thejoeflynn@gmail.com>
2026-05-29 14:48:30 -0400

    Added Interface and Vehicle files

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/interfaces/Rideable.java
@@ -0,0 +1,6 @@
+package com.zipcodewilmington.froilansfarm.interfaces;
+
+public interface Rideable {
+    void beMounted(Rider rider);
+    void beDismounted(Rider rider);
+}
---
+++ src/main/java/com/zipcodewilmington/froilansfarm/vehicles/Tractor.java
@@ -0,0 +1,33 @@
+package com.zipcodewilmington.froilansfarm.vehicles;
+
+import com.zipcodewilmington.froilansfarm.farm.Farm;
+import com.zipcodewilmington.froilansfarm.crops.Crop;
+import com.zipcodewilmington.froilansfarm.interfaces.Rider;
+
+public class Tractor extends FarmVehicle {
+
+    public Tractor() {
+        super("Tractor");
+    }
+
+    @Override
+    public String makeNoise() {
+        return "Vroom";
+    }
+
+    @Override
+    public void operate(Farm farm) {
+    }
+
+    @Override
+    public void beMounted(Rider rider) {
+    }
+
+    @Override
+    public void beDismounted(Rider rider) {
+    }
+
+    public void harvest(Crop crop) {
+        crop.harvest();
+    }
+}
