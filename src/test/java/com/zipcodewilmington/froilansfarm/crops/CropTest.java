eb676b57c4bc4b086e19c8f7c049e6799c6c4fde
prachi0716 <prachiptl1707@gmail.com>
2026-05-29 09:58:24 -0400

    InnHorseTest  added eat, mount, and dismount tests

--- src/test/java/com/zipcodewilmington/froilansfarm/crops/CropTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/crops/CropTest.java
@@ -8,35 +8,43 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CropTest {
 
     @Test
-    void cropCanBeFertilized() {
+    public void cropCanBeFertilizedTest() {
         Crop crop = new CornStalk(false);
+
         crop.fertilize();
+
         assertTrue(crop.hasBeenFertilized());
     }
 
     @Test
-    void cropStartsUnfertilized() {
+    public void cropStartsUnfertilizedTest() {
         Crop crop = new CornStalk(false);
+
         assertFalse(crop.hasBeenFertilized());
     }
 
     @Test
-    void cropStartsUnharvested() {
+    public void cropStartsUnharvestedTest() {
         Crop crop = new CornStalk(false);
+
         assertFalse(crop.hasBeenHarvested());
     }
 
     @Test
-    void cropCanBeHarvested() {
+    public void cropCanBeHarvestedTest() {
         Crop crop = new CornStalk(false);
+
         crop.harvest();
+
         assertTrue(crop.hasBeenHarvested());
     }
 
     @Test
-    void unfertilizedCropDoesNotYield() {
+    public void unfertilizedCropDoesNotYieldTest() {
         Crop crop = new CornStalk(false);
+
         Edible result = crop.yield();
+
         assertNull(result);
     }
 }
