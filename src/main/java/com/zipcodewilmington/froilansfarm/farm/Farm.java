0d58556adec1894ee4205948134b3cbf136f6d94
gitanitraj <anitra.com@gmail.com>
2026-05-31 13:55:30 -0400

    Updates to align with tests

--- src/main/java/com/zipcodewilmington/froilansfarm/farm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/Farm.java
@@ -1,8 +1,17 @@
 package com.zipcodewilmington.froilansfarm.farm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zipcodewilmington.froilansfarm.interfaces.Aircraft;
+import com.zipcodewilmington.froilansfarm.vehicles.FarmVehicle;
+
 public class Farm {
 
     private Field field;
+
+    private final List<FarmVehicle> vehicles = new ArrayList<>();
+    private final List<Aircraft> aircraft = new ArrayList<>();
 
     public Field getField() {
         return field;
@@ -10,5 +19,13 @@ public class Farm {
 
     public void setField(Field field) {
         this.field = field;
+    }
+
+    public void addVehicle(FarmVehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+
+    public void addAircraft(Aircraft aircraft) {
+        this.aircraft.add(aircraft);
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/farm/Stable.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/Stable.java
@@ -1,5 +1,19 @@
 package com.zipcodewilmington.froilansfarm.farm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zipcodewilmington.froilansfarm.animal.Horse;
+
 public class Stable {
-    
+
+    private final List<Horse> horses = new ArrayList<>();
+
+    public void add(Horse horse) {
+        horses.add(horse);
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
 }
