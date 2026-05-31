7237147be1af3259dced54afa9fd232db3119852
gitanitraj <anitra.com@gmail.com>
2026-05-31 13:55:30 -0400

    Updates to align with tests

--- src/main/java/com/zipcodewilmington/froilansfarm/farm/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/ChickenCoop.java
@@ -1,5 +1,19 @@
 package com.zipcodewilmington.froilansfarm.farm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zipcodewilmington.froilansfarm.animal.Chicken;
+
 public class ChickenCoop {
-    
+
+    private final List<Chicken> chickens = new ArrayList<>();
+
+    public void add(Chicken chicken) {
+        chickens.add(chicken);
+    }
+
+    public List<Chicken> getChickens() {
+        return chickens;
+    }
 }
