764baae76357f074b6a1946aad198eccc19c5206
gitanitraj <anitra.com@gmail.com>
2026-05-31 12:13:33 -0400

    Made updates for testing purposes

--- src/main/java/com/zipcodewilmington/froilansfarm/crop/Yields/EarCorn.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/crop/Yields/EarCorn.java
@@ -2,5 +2,8 @@ package com.zipcodewilmington.froilansfarm.crop.Yields;
 
 import com.zipcodewilmington.froilansfarm.animal.Edible;
 
+import com.zipcodewilmington.froilansfarm.interfaces.Edible;
+
 public class EarCorn implements Edible {
+
 }
